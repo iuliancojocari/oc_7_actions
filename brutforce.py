0835dabb511ace66bd821f042dbfe6a90244439f
IulianK <iuliancojocari42@gmail.com>
2022-12-06 11:43:36 +0100

    brutforce finished

--- brutforce.py
+++ brutforce.py
@@ -7,8 +7,8 @@ csv_brutforce = os.path.join(ROOT_DIR, "data\\optimized.csv")
 
 def get_csv_data(file):
     """ Get data from csv file
-    :param file: file name
-    :return: list of tuples
+    @param file: file name
+    @return: list of tuples
     """
     with open(file, newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -16,7 +16,7 @@ def get_csv_data(file):
         next(csv_reader)
 
         for row in csv_reader:
-            action_name = row[0]
+            stock_name = row[0]
             
             # convert price from € to cents
             price_in_cents = float(row[1]) * 100
@@ -24,20 +24,24 @@ def get_csv_data(file):
             # calculate benefit in cents
             benefit_in_cents = float(row[2]) * 100
             
-
             # create tuple for each row with formated data
-            formated_data.append((action_name, int(price_in_cents), int(benefit_in_cents)))
+            formated_data.append((stock_name, int(price_in_cents), int(benefit_in_cents)))
 
         return formated_data
 
 
-def generate_combinations(actions):
+def generate_combinations(stocks):
+    """ Generate all cominations with the given elements
+    
+    @param actions: list of tuples (tuple = stock)
+    @return list: best stocks combinations
+    """
     client_budget = 500 * 100
     profit = 0
     best_combination = []
     
-    for i in range(len(actions)):
-        list_combinations = combinations(actions, i+1)
+    for i in range(len(stocks)):
+        list_combinations = combinations(stocks, i+1)
 
         for combination in list_combinations:
             total_cost = calculate_cost(combination)
@@ -47,40 +51,49 @@ def generate_combinations(actions):
 
                 if total_profit > profit:
                     profit = total_profit
-
                     best_combination = combination
                     
     return best_combination
 
 def calculate_profit(combination):
-    
+    """ Calculate the profit of all stocks of the combination
+    @param combination: list of stocks
+    @return int: all stocks profit
+    """
     profit = []
 
-    for action in combination:
-        profit.append(action[2])
+    for stock in combination:
+        profit.append(stock[2])
 
     return sum(profit)
 
-
 def calculate_cost(combination):
+    """ Calculate the cost of all stocks of the combination
+    @param combination: list of stocks
+    @return int: all stocks price
+    """
     price = []
 
-    for action in combination:
-        price.append(action[1])
+    for stock in combination:
+        price.append(stock[1])
 
     return sum(price)
 
 def display_result(best_combination):
+    """ Display informations about the best combination
+    
+    @param best_combination: list of stocks
+    """
     print("Liste des actions achetées :\n")
 
-    for action in best_combination:
-        print(f"{action[0]} {action[1] / 100}€ {action[2] / 100}€")
+    for stock in best_combination:
+        print(f"{stock[0]} {stock[1] / 100}€ {stock[2] / 100}€")
 
     print(f"\nSomme dépensée : {calculate_cost(best_combination) / 100}€")
     print(f"Profit total : {calculate_profit(best_combination) / 100}€")
 
 
 if __name__ == "__main__":
-    actions = get_csv_data(csv_brutforce)
-    best_combination = generate_combinations(actions)
+    stocks = get_csv_data(csv_brutforce)
+    best_combination = generate_combinations(stocks)
     display_result(best_combination)
