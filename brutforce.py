53fb7b3caa93f1c628ffe269262e3a33c85fe9b1
IulianK <iuliancojocari42@gmail.com>
2022-12-13 14:40:00 +0100

    final version

--- brutforce.py
+++ brutforce.py
@@ -1,82 +1,59 @@
 import csv
 from itertools import combinations
 
-def get_csv_data():
-    """ Get data from csv file
+MAX_INVEST = 500 * 100
 
-    @return: list of tuples
+
+def get_csv_data():
     """
-    with open("data\\brutforce.csv", newline='') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        formated_data = []
+    Get data from csv file
+
+    :return: list of tuples
+    """
+    with open("data\\brutforce.csv", newline="") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
         next(csv_reader)
 
         for row in csv_reader:
             stock_name = row[0]
-            
+
             # convert price from € to cents
             price_in_cents = float(row[1]) * 100
 
             # calculate benefit in cents
             benefit_in_cents = float(row[2]) * 100
-            
-            # create tuple for each row with formated data
-            formated_data.append((stock_name, int(price_in_cents), int(benefit_in_cents)))
 
-        return formated_data
+            yield (stock_name, int(price_in_cents), int(benefit_in_cents))
 
 
 def generate_combinations(stocks):
-    """ Generate all cominations with the given elements
-    
-    @param actions: list of tuples (tuple = stock)
-    @return list: best stocks combinations
     """
-    client_budget = 500 * 100
+    Generate all cominations with the given elements
+
+    :param actions: list of tuples (tuple = stock)
+    :return list: best stocks combinations
+    """
     profit = 0
     best_combination = []
-    
+
     for i in range(len(stocks)):
-        list_combinations = combinations(stocks, i+1)
+        list_combinations = combinations(stocks, i + 1)
 
         for combination in list_combinations:
-            total_cost = calculate_cost(combination)
+            total_cost = sum([stock[1] for stock in combination])
 
-            if total_cost <= client_budget:
-                total_profit = calculate_profit(combination)
+            if total_cost <= MAX_INVEST:
+                total_profit = sum([stock[2] for stock in combination])
 
                 if total_profit > profit:
                     profit = total_profit
                     best_combination = combination
-                    
+
     return best_combination
 
-def calculate_profit(combination):
-    """ Calculate the profit of all stocks of the combination
-    @param combination: list of stocks
-    @return int: all stocks profit
-    """
-    profit = []
-
-    for stock in combination:
-        profit.append(stock[2])
-
-    return sum(profit)
-
-def calculate_cost(combination):
-    """ Calculate the cost of all stocks of the combination
-    @param combination: list of stocks
-    @return int: all stocks price
-    """
-    price = []
-
-    for stock in combination:
-        price.append(stock[1])
-
-    return sum(price)
 
 def display_result(best_combination):
-    """ Display informations about the best combination
+    """Display informations about the best combination
 
     @param best_combination: list of stocks
     """
@@ -85,11 +62,11 @@ def display_result(best_combination):
     for stock in best_combination:
         print(f"{stock[0]} {stock[1] / 100}€ {stock[2] / 100}€")
 
-    print(f"\nSomme dépensée : {calculate_cost(best_combination) / 100}€")
-    print(f"Profit total : {calculate_profit(best_combination) / 100}€")
+    print(f"\nSomme dépensée : {sum([stock[1] for stock in best_combination]) / 100}€")
+    print(f"Profit total : {sum([stock[2] for stock in best_combination]) / 100}€")
 
 
 if __name__ == "__main__":
-    stocks = get_csv_data()
+    stocks = [stock for stock in get_csv_data()]
     best_combination = generate_combinations(stocks)
     display_result(best_combination)
--- optimized.py
+++ optimized.py
@@ -1,63 +1,94 @@
 import csv
 
-def get_csv_data():
-    """ Get data from csv file
+MAX_INVEST = 500 * 100
 
-    @return: list of tuples
+
+def get_csv_data():
     """
-    with open("data\\optimized.csv", newline='') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        formated_data = []
+    Get data from csv file
+
+    :return: list of tuples
+    """
+    with open("data\\optimized.csv", newline="") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
         next(csv_reader)
 
         for row in csv_reader:
+
             stock_name = row[0]
-            
+
             # convert price from € to cents
             price_in_cents = float(row[1]) * 100
 
             # calculate benefit in cents
             benefit_in_cents = float(row[2]) * 100
-            
-            # create tuple for each row with formated data
-            formated_data.append((stock_name, int(price_in_cents), int(benefit_in_cents)))
 
-        return formated_data
+            if price_in_cents <= 0 or benefit_in_cents <= 0:
+                continue
 
-def knapSack(max_invest, cost, profit, total_actions, actions): 
+            yield (stock_name, int(price_in_cents), int(benefit_in_cents))
+
+
+def knap_sack(max_invest, actions):
+    """
+    Knapsack algorithm
+
+    :param max_invest: Customer budget
+    :param stocks: List of stocks
+
+    :returns: Best combination of stocks
+    """
+    profit = [v[2] for v in actions]
+    cost = [w[1] for w in actions]
+    total_actions = len(profit)
+
     matrix = [[0 for x in range(max_invest + 1)] for x in range(total_actions + 1)]
-    
-    # Build table K[][] in bottom up manner 
-    for i in range(total_actions + 1): 
-        for w in range(max_invest + 1): 
-            if i == 0 or w == 0: 
+
+    # Build table K[][] in bottom up manner
+    for i in range(total_actions + 1):
+        for w in range(max_invest + 1):
+            if i == 0 or w == 0:
                 matrix[i][w] = 0
-            elif cost[i-1] <= w:
-                 
-                matrix[i][w] = max(profit[i-1] + matrix[i-1][w-cost[i-1]], matrix[i-1][w]) 
-                                
-                
-            else: 
-                matrix[i][w] = matrix[i-1][w] 
-      
+            elif cost[i - 1] <= w:
+
+                matrix[i][w] = max(
+                    profit[i - 1] + matrix[i - 1][w - cost[i - 1]], matrix[i - 1][w]
+                )
+
+            else:
+                matrix[i][w] = matrix[i - 1][w]
+
     best_combination = []
     while max_invest >= 0 and total_actions >= 0:
 
-        if matrix[total_actions][max_invest] == \
-            matrix[total_actions-1][max_invest - cost[total_actions-1]] + profit[total_actions-1]:
+        if (
+            matrix[total_actions][max_invest]
+            == matrix[total_actions - 1][max_invest - cost[total_actions - 1]]
+            + profit[total_actions - 1]
+        ):
 
             best_combination.append(actions[total_actions - 1])
-            max_invest -= cost[total_actions-1]
+            max_invest -= cost[total_actions - 1]
 
         total_actions -= 1
 
-    return sum([float(c[1] / 100) for c in best_combination])
-   
+    return best_combination
 
-actions = get_csv_data()
 
-profit = [v[2] for v in actions]  # profit
-cost = [w[1] for w in actions] # cost
-max_invest = 50000 # max invest
-total_actions = len(profit) 
-print(knapSack(max_invest, cost, profit, total_actions, actions))
+def display_results():
+    """
+    Display results
+    """
+    stocks = [stock for stock in get_csv_data()]
+    best_combination = knap_sack(MAX_INVEST, stocks)
+
+    print("Liste des actions achetées : \n")
+    for stock in best_combination:
+        print(f"{stock[0]} - {stock[1] / 100}€ - {stock[2] / 100}€")
+
+    print(f"Total dépensé : {sum([stock[1] for stock in best_combination]) / 100}€")
+    print(f"Profit total : {sum([stock[2] for stock in best_combination]) / 100}€")
+
+
+if __name__ == "__main__":
+    display_results()
